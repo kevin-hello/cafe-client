1b334872cc75bbf13fa8a840673a862f05b56ae4
Kevin Schaefer <kevinss.hello@gmail.com>
2022-06-27 15:46:53 -0700

    areas list code removed, test

--- src/components/areas-list/areas-list.jsx
+++ src/components/areas-list/areas-list.jsx
@@ -4,19 +4,19 @@ import { connect } from 'react-redux';
 import { AreaCard } from "../area-card/area-card";
 
 
-function AreasList(props) {
-  const { cafes } = props;
-  let filteredAreas = cafes;
+// function AreasList(props) {
+//   const { cafes } = props;
+//   let filteredAreas = cafes;
 
 
-if (!cafes) return <div className="main-view"/>;
+// if (!cafes) return <div className="main-view"/>;
 
-return <>
-    {filteredAreas.map(c=> (
-    <Col className="card-div"sm={12} md={6} lg={4} key={c.Area.Name}>
-      <AreaCard cafe={c}/>
-    </Col>
-    ))}
-</>;
-}
-export default AreasList;
+// return <>
+//     {filteredAreas.map(c=> (
+//     <Col className="card-div"sm={12} md={6} lg={4} key={c.Area.Name}>
+//       <AreaCard cafe={c}/>
+//     </Col>
+//     ))}
+// </>;
+// }
+// export default AreasList;
