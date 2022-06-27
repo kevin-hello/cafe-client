d1390a6568214beba00325c91211b04857445750
Kevin Schaefer <kevinss.hello@gmail.com>
2022-06-27 15:19:56 -0700

    area view content adjusted

--- src/components/area-view/area-view.jsx
+++ src/components/area-view/area-view.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 //UI elements
-import { Row, Col, Button, Container} from 'react-bootstrap';
+import { Row, Container, Col, Button} from 'react-bootstrap';
 
 import { CafeCard } from '../cafe-card/cafe-card';
 //styling
@@ -11,18 +11,28 @@ import "./area-view.scss";
 export function AreaView ({cafes, cafe, onBackClick}) {
   
   return(
-    <>
+    <Container>
+      <Row>
     <Col className="area-view">
       <div className="area-name">
         <h1>{cafe.Area.Name}</h1>
       </div>
+      <div className="area-description">
+        <span>{cafe.Area.Description}</span>
+      </div>
+      <Button id="return" variant="primary" onClick={() => onBackClick()}>Back</Button>
     </Col>
+    </Row>
+    <Row className='area-cafes'>
+      <h3>{cafe.Area.Name} Cafes</h3>
+    </Row>
+    <Row>
     { cafes && cafes.map((cafe) => (
-    <Col className='area-cafe-cards' md={3} key={cafe._id}>
+    <Col className='area-cafe-cards' md={4} key={cafe._id}>
     <CafeCard cafe={cafe} />
     </Col> ))}
-          <Button id="return" variant="primary" onClick={() => onBackClick()}>Back</Button>
-    </>
+    </Row>
+    </Container>
   );
 }
 
