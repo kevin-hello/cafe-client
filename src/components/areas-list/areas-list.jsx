fa7ba88bde79dccdbab6852a56a7433c7d945029
Kevin Schaefer <kevinss.hello@gmail.com>
2022-06-29 13:11:06 -0700

    uniqueAreas variable adjusted

--- src/components/areas-list/areas-list.jsx
+++ src/components/areas-list/areas-list.jsx
@@ -4,11 +4,11 @@ import { AreaCard } from '../area-card/area-card';
 
 function AreasList(props) {
 
-const uniqueAreas = [...new Set(props.cafes.Area.Name)];
+const uniqueAreas = [...new Set(props.cafes.Area)];
 
 console.log(uniqueAreas);
 return ( <>
-    {uniqueAreas.map((cafe, index)=> <Col key={cafe.Area.Name}>
+    {uniqueAreas.map((cafe, index)=> <Col>
         <AreaCard cafe={cafe}/>
         </Col>)}
 </>
