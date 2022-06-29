1aa347fcc4b2d4542b89028d469b2404d85ffc43
Kevin Schaefer <kevinss.hello@gmail.com>
2022-06-29 15:39:23 -0700

    areaslist import adjusted

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -9,7 +9,7 @@ import { RegistrationView } from "../registration-view/registration-view";
 import { AreaView } from "../area-view/area-view";
 // import { ProfileView } from "../profile-view/profile-view";
 import { Menubar } from "../navbar/menubar";
-import { AreasList } from "../areas-list/areas-list";
+import AreasList from "../areas-list/areas-list";
 import { Row, Col, Container } from "react-bootstrap";
 
 import { setCafes } from '../../actions/actions';
