d2ea200ce6da8791c6176a02fb4ac79f5017465c
Kevin Schaefer <kevinss.hello@gmail.com>
2022-06-29 14:05:46 -0700

    areas-list filtering

--- src/components/areas-list/areas-list.jsx
+++ src/components/areas-list/areas-list.jsx
@@ -4,7 +4,12 @@ import { AreaCard } from '../area-card/area-card';
 
 function AreasList(props) {
 
-        console.log(props.cafes);
+console.log(props.cafes);
+
+const uniqueAreasSet = new Set(props.cafes.map((c) => c.Area.Name));
+const uniqueAreas = Array.from(uniqueAreasSet).sort();
+
+console.log(uniqueAreas);
 
 return ( <>
     {props.cafes.map((cafe, index)=> <Col key={cafe.Area.Name}>
