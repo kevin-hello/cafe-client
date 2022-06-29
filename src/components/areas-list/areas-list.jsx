a4783f62953c8efc9aa960dbe6cefffc68b7e4b4
Kevin Schaefer <kevinss.hello@gmail.com>
2022-06-29 15:35:54 -0700

    areas list reverted to function

--- src/components/areas-list/areas-list.jsx
+++ src/components/areas-list/areas-list.jsx
@@ -2,20 +2,12 @@ import React from 'react';
 import { Col } from 'react-bootstrap';
 import { AreaCard } from '../area-card/area-card';
 
-export class AreasList extends React.Component {
-
-constructor(props) {
-  super(props);
-}
-
-  render() {
-    const {cafes} = this.props;
-    return (
-      <>
-    {props.cafes.map((cafe, index)=> <Col key={cafe.Area.Name}>
+function AreasList(props) {
+return(<>
+    {cafes.map((cafe, index)=> <Col key={cafe.Area.Name}>
         <AreaCard cafe={cafe}/>
         </Col>)}
-</>
-    );
-  }
+</>)
 }
+export default AreasList;
+
