195dd76ab6acbe539d0eaddec53ced437f6361c7
Kevin Schaefer <kevinss.hello@gmail.com>
2022-06-28 22:22:09 -0700

    area list return moved

--- src/components/areas-list/areas-list.jsx
+++ src/components/areas-list/areas-list.jsx
@@ -4,12 +4,10 @@ import { AreaCard } from '../area-card/area-card';
 
 function AreasList(props) {
 
-return ( 
-<>
+return ( <>
     {props.cafes.map((cafe, index)=> <Col key={cafe.Area.Name}>
         <AreaCard cafe={cafe}/>
         </Col>)}
 </>
-)
-}
+)}
 export default AreasList;
